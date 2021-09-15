cedf1fafd2a6508e937e00461bb5170aaca98f2c
Jantje Joppien <jantjej.99@gmail.com>
2021-09-15 19:43:04 +0200

    add pagination

--- src/main/kotlin/com/fleur/cookbook/data/repositories/IngredientRepository.kt
+++ src/main/kotlin/com/fleur/cookbook/data/repositories/IngredientRepository.kt
@@ -1,9 +1,9 @@
 package com.fleur.cookbook.data.repositories
 
 import com.fleur.cookbook.data.models.Ingredient
-import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.PagingAndSortingRepository
 
 
-interface IngredientRepository : JpaRepository<Ingredient?, Long?> {
-    fun findAllByName(name: String?): List<Ingredient?>?
+interface IngredientRepository : PagingAndSortingRepository<Ingredient?, Long?> {
+    fun findAllByName(name: String): List<Ingredient?>
 }
--- src/main/kotlin/com/fleur/cookbook/rest/service/IngredientService.kt
+++ src/main/kotlin/com/fleur/cookbook/rest/service/IngredientService.kt
@@ -3,6 +3,8 @@ package com.fleur.cookbook.rest.service
 import com.fleur.cookbook.data.models.Ingredient
 import com.fleur.cookbook.data.repositories.IngredientRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Sort
 import org.springframework.stereotype.Service
 
 
@@ -11,9 +13,10 @@ class IngredientService {
     @Autowired
     lateinit var ingredientRepository: IngredientRepository
 
-    fun getAllIngredients(): MutableList<Ingredient?> = ingredientRepository.findAll()
+    fun getAllIngredients(page: Int, size: Int): MutableIterable<Ingredient?> =
+        ingredientRepository.findAll(PageRequest.of(page, size, Sort.by("name")))
 
-    fun findIngredientByName(name: String?) = ingredientRepository.findAllByName(name)
+    fun findIngredientByName(name: String) = ingredientRepository.findAllByName(name).sortedBy { it?.id }
 
     fun saveIngredient(ingredient: Ingredient) = ingredientRepository.save(ingredient)
 }
